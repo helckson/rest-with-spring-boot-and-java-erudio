e161a3487f688c567d017b3bbc99f920e38e1f33
helckson <helckson.ishibashi@hotmail.com>
2023-01-08 20:59:45 -0300

    API versioning

--- 05-FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 05-FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.mapper.DozerMapper;
+import br.com.erudio.mapper.custom.PersonMapper;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 
@@ -19,6 +21,9 @@ public class PersonServices {
 	
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PersonMapper mapper;
 	
 	public List<PersonVO> findAll() {
 		
@@ -42,6 +47,14 @@ public class PersonServices {
 			logger.info("Create on person!");
 			var entity = DozerMapper.parseObject(person, Person.class);
 			var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+			return vo;
+		}
+		
+		public PersonVOV2 createV2(PersonVOV2 person) {
+			
+			logger.info("Create on person with V2!");
+			var entity = mapper.converterVoTOEntity(person);
+			var vo =  mapper.converterEntityToVo(repository.save(entity));
 			return vo;
 		}
 		
