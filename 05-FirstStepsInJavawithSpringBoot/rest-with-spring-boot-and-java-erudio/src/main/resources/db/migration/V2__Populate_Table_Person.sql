8607314667ec2de4e7c3a3196d7a340d46fa796c
helckson <helckson.ishibashi@hotmail.com>
2023-01-31 17:40:08 -0300

    Migrations Flyway

---
+++ 05-FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `person` (`id`, `address`, `firt_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ronaldo', 'Male', 'Fenômeno'),
+	(2, 'Rio de Janeiro', 'Ronaldinho', 'Male', 'Gaúcho'),
+	(4, 'São Paulo', 'Angel', 'Male', 'Romero'),
+	(5, 'São Paulo', 'Renato', 'Male', 'Augusto'),
+	(7, 'Estados Unidos', 'Anderson ', 'Male', 'Silva'),
+	(8, 'Espanha', 'Rafael', 'Male', 'Nadal'),
+	(9, 'Estados Unidos', 'Lebron', 'Male', 'James');
